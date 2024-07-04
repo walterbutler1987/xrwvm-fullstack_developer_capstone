3e26e4d709b7adfe35408f105f26524d89ee89b5
walterbutler1987 <walterbutler1987@gmail.com>
2024-07-03 21:37:40 -0400

    Updating with latest changes

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -36,3 +36,4 @@ def initiate():
 
     for data in car_model_data:
             CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+            
