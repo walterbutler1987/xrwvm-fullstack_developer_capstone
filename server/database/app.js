f03e15d09949822a5d51f521d2935298e81ea849
walterbutler1987 <walterbutler1987@gmail.com>
2024-07-03 21:53:07 -0400

    Updating with latest changes

--- server/database/app.js
+++ server/database/app.js
@@ -21,10 +21,10 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
   
 } catch (error) {
