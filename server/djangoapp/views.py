0ec0ec5da54e25189d7e68527bd246f586676dac
walterbutler1987 <walterbutler1987@gmail.com>
2024-07-03 22:00:45 -0400

    Updating with latest changes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,16 +18,14 @@ from .populate import initiate
 from .models import CarMake, CarModel
 from .restapis import get_request, analyze_review_sentiments, post_review
 
-
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
-
 
 # Create your views here.
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
-def login_user(request):
+def login_user(request): 
     # Get username and password from request.POST dictionary
     data = json.loads(request.body)
     username = data['userName']
@@ -35,21 +33,21 @@ def login_user(request):
     # Try to check if provide credential can be authenticated
     user = authenticate(username=username, password=password)
     data = {"userName": username}
-    if user is not None:
+    if user is not None: 
         # If user is valid, call login method to login current user
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-def logout_request(request):
+def logout_request(request): 
     logout(request)
     data = {"userName":""}
     return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
 @csrf_exempt
-def registration(request):
+def registration(request): 
     context = {}
 
     data = json.loads(request.body)
@@ -60,30 +58,30 @@ def registration(request):
     email = data['email']
     username_exist = False
     email_exist = False
-    try:
+    try: 
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except: 
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
     # If it is a new user
-    if not username_exist:
+    if not username_exist: 
         # Create user in auth_user table
         user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName":username,"status":"Authenticated"}
         return JsonResponse(data)
-    else :
+    else : 
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
 
-def get_cars(request):
+def get_cars(request): 
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if(count == 0): 
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
@@ -93,16 +91,16 @@ def get_cars(request):
 
 #Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if(state == "All"): 
         endpoint = "/fetchDealers"
-    else:
+    else: 
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
     return JsonResponse({"status":200,"dealers":dealerships})
 
-def get_dealer_reviews(request, dealer_id):
+def get_dealer_reviews(request, dealer_id): 
     # if dealer id has been provided
-    if(dealer_id):
+    if(dealer_id): 
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
         for review_detail in reviews:
@@ -113,21 +111,21 @@ def get_dealer_reviews(request, dealer_id):
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 
-def get_dealer_details(request, dealer_id):
-    if(dealer_id):
+def get_dealer_details(request, dealer_id): 
+    if(dealer_id): 
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
         return JsonResponse({"status":200,"dealer":dealership})
-    else:
+    else: 
         return JsonResponse({"status":400,"message":"Bad Request"})
 
-def add_review(request):
-    if(request.user.is_anonymous == False):
+def add_review(request): 
+    if(request.user.is_anonymous == False): 
         data = json.loads(request.body)
-        try:
+        try: 
             response = post_review(data)
             return JsonResponse({"status":200})
-        except:
+        except: 
             return JsonResponse({"status":401,"message":"Error in posting review"})
-    else:
+    else: 
         return JsonResponse({"status":403,"message":"Unauthorized"})
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,10 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://walterbutler-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://walterbutler-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS =
+    ['localhost','https://walterbutler-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS=
+    ['https://walterbutler-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -93,20 +95,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
     },
 ]
 
@@ -143,4 +145,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
