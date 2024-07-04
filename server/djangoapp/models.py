1008e231abe8a27cdab266fc140b4a358fdd88cd
walterbutler1987 <walterbutler1987@gmail.com>
2024-07-03 22:08:19 -0400

    Updating with latest changes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -32,7 +32,7 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
@@ -43,10 +43,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-              validators=[
+                               validators=[
                 MaxValueValidator(2023),
                 MinValueValidator(2015)
-            ])
+                                ])
     # Other fields as needed
 
     def __str__(self):
